66b494c09fad252175c1a3e9600e1b3804bf73c3
Marc-Oliver Körner <Marc-Oliver.Koerner@training.lug-ag.de>
2024-09-05 15:23:27 +0200

    Projektdateien hinzufügen.

---
+++ Points.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MyQuiz
+{
+	public class Points : INotifyPropertyChanged
+	{
+		private int _pts;
+
+		public int Pts
+		{
+			get => _pts;
+			set
+			{
+				if(_pts != value)
+				{
+					_pts = value;
+					OnPropertyChanged();
+				}
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
